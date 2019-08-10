60151e97771f8af91e39dd7207f2c62a614393ba
Callicodes <callicodes@gmail.com>
2019-08-10 23:27:35 +0100

    Created The Header Component

---
+++ zone-movie-app/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="rmdb-header">
+      <div className="rmdb-header-content">
+        <img
+          className="rmdb-logo"
+          src="./images/zoneLogo.svg"
+          alt="rmdb-logo"
+        />
+        <img
+          className="rmdb-tmdb-logo"
+          src="./images/tmdb_logo.png"
+          alt="tmdb-logo"
+        />
+      </div>
+    </div>
+  );
+};
+export default Header;
