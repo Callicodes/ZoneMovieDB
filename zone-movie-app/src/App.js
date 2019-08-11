f92cbbd2a831c00adafd1def0e21ed8dfecc0061
Callicodes <callicodes@gmail.com>
2019-08-11 12:15:01 +0100

    Add several <Main /> components for each section

--- zone-movie-app/src/App.js
+++ zone-movie-app/src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
-import { PATH_POPULAR } from "./api/api";
-
+// import { PATH_POPULAR } from "./api/api";
+import { PATH_POPULAR, PATH_TOP_RATED, PATH_GENRE } from "./api/api";
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,8 @@ function App() {
         <div className="App-content-wrapper">
           {/* <Main title="Popular" /> */}
           <Main title="Popular" section={PATH_POPULAR} />
+          <Main title="Top Rated" section={PATH_POPULAR} />
+          <Main title="Coming Soon" section={PATH_GENRE} />
           {/* <h1 className="App-main-title">Main Title</h1> */}
         </div>
       </div>
