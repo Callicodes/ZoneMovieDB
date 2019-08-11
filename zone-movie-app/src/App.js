1f7431da583d2627a70e70803900d80b091e30bc
Callicodes <callicodes@gmail.com>
2019-08-11 10:01:09 +0100

    Added Sidebar component
    App.js + App.css + Main.js

--- zone-movie-app/src/App.js
+++ zone-movie-app/src/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="App-main">
+        <div className="App-sidebar-wrapper">
+          <Sidebar />
+        </div>
+        <div className="App-content-wrapper">
+          <h1 className="App-main-title">Main Title</h1>
+        </div>
+      </div>
     </div>
   );
 }
